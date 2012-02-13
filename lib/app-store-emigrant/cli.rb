0e67dc11cfde510159259d0036dc75cea84a56d7
Tim Kurvers <tim@moonsphere.net>
2012-02-13 21:47:54 +0100

    Added initial command-line implementation

---
+++ lib/app-store-emigrant/cli.rb
@@ -0,0 +1,18 @@
+
+module AppStore
+  
+  module Emigrant
+    
+    class CLI
+      
+      def initialize args
+        
+        p 'initializing', args
+        
+      end
+      
+    end
+    
+  end
+  
+end
