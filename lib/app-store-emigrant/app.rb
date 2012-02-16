35abac09e881f9a80732a4213dd1c74e71ce7815
Tim Kurvers <tim@moonsphere.net>
2012-02-16 19:37:52 +0100

    Added Library and App classes

---
+++ lib/app-store-emigrant/app.rb
@@ -0,0 +1,18 @@
+
+module AppStore
+  
+  module Emigrant
+    
+    class App
+      
+      def initialize path
+        
+        
+        
+      end
+      
+    end
+    
+  end
+  
+end
---
+++ lib/app-store-emigrant/library.rb
@@ -0,0 +1,20 @@
+
+module AppStore
+  
+  module Emigrant
+    
+    class Library
+      
+      attr_reader :apps
+      
+      def initialize path
+        
+        @apps = []
+        
+      end
+      
+    end
+    
+  end
+  
+end
