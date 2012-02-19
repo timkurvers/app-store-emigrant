479a8a97ce12b3b36b63ec3fe18513f4ebcb8791
Tim Kurvers <tim@moonsphere.net>
2012-02-19 22:11:01 +0100

    Added initial implementation for App and Library

--- lib/app-store-emigrant/app.rb
+++ lib/app-store-emigrant/app.rb
@@ -1,13 +1,113 @@
+require 'cfpropertylist'
+require 'json'
+require 'net/http'
+require 'zip/zip'
+require 'zip/zipfilesystem'
 
 module AppStore; end
 
 module AppStore::Emigrant
-    
+  
+  # Represents a single iTunes mobile application
   class App
     
+    # List of valid extensions for an application
+    VALID_EXTENSIONS = [
+      '.ipa'
+    ]
+    
+    # Regular expression to match a version number in filenames
+    FILENAME_VERSION_REGEX = Regexp.new('(?<version>[0-9.]+)(?:' + VALID_EXTENSIONS.join('|') + ')')
+    
+    attr_reader :path
+    
+    # Initializes application from given path
     def initialize path
+      @path = Pathname.new path
+      @path = @path.expand_path unless @path.absolute?
       
+      # Ensure application exists
+      unless @path.file?
+        raise DoesNotExist, "Given path is not a valid application: #{@path}"
+      end
+      
+      # Ensure application has valid extension
+      raise Invalid unless VALID_EXTENSIONS.include? @path.extname
+      
+      @metadata = nil
+      @clouddata = nil
     end
+    
+    # Filename of this application (including extension)
+    def filename
+      unless @filename
+        @filename = @path.basename().to_s
+      end
+      
+      @filename
+    end
+    
+    # Name of this application (from its metadata)
+    def name
+      metadata['itemName']
+    end
+    
+    # Version of this application
+    def version
+      unless @version
+        
+        # Extract version information (if available)
+        @version = metadata['bundleShortVersionString'] || metadata['bundleVersion']
+        
+        # Otherwise, use the filename
+        unless @version
+          @version = filename[FILENAME_VERSION_REGEX, :version]
+        end
+        
+      end
+      
+      @version
+    end
+    
+    # Lazily loads local metadata for this application from its iTunesMetadata.plist
+    def metadata
+      unless @metadata
+        Zip::ZipFile.open(@path) do |zip|
+          data = zip.file.read('iTunesMetadata.plist')
+          plist = CFPropertyList::List.new(data: data)
+          @metadata = CFPropertyList.native_types(plist.value)
+        end
+      end
+      
+      @metadata
+    end
+    
+    # Lazily queries Apple's iTunes Store API for latest cloud data
+    # Note: Clouddata may be nil if the application was removed from the store
+    def clouddata
+      unless @clouddata
+        response = Net::HTTP.get('itunes.apple.com', '/lookup?id=' + metadata['itemId'].to_s)
+        @clouddata = JSON.parse(response)['results'].first
+      end
+      
+      @clouddata
+    end
+    
+    # Version available in the cloud
+    def cloudversion
+      clouddata && clouddata['version']
+    end
+    
+    # Whether this application is outdated
+    def outdated?
+      return cloudversion && version != cloudversion
+    end
+  
+    # Raised when an application does not exist
+    class DoesNotExist < StandardError; end
+  
+    # Raised when an application is invalid (e.g non-valid extension)
+    class Invalid < StandardError; end
     
   end
   
--- lib/app-store-emigrant/library.rb
+++ lib/app-store-emigrant/library.rb
@@ -1,27 +1,87 @@
-
 require 'pathname'
 
 module AppStore; end
 
 module AppStore::Emigrant
-    
+  
+  # Represents a single iTunes mobile applications library
   class Library
     
-    attr_reader :path, :apps
+    attr_reader :path
     
+    # Initializes library from given path
     def initialize path
-      
       @path = Pathname.new path
       @path = @path.expand_path unless @path.absolute?
       
+      # Ensure library is a valid directory
       unless @path.directory?
-        raise Library::DoesNotExist, "Given path is not a valid library directory: #{@path}"
+        raise DoesNotExist, "Given path is not a valid library directory: #{@path}"
       end
       
-      @apps = []
-      
+      @apps = nil
     end
     
+    # Retrieves a list of applications
+    def apps
+      unless @apps
+        load!
+      end
+      
+      @apps
+    end
+    
+    # Force-loads applications from disk
+    def load!
+      @apps = []
+      @path.each_child do |item|
+        if item.file?
+          begin
+            @apps << App.new(item)
+          rescue App::Invalid; end
+        end
+      end
+      self
+    end
+    
+    # Searches for applications containing given snippet in filename
+    def find snippet
+      apps.select do |app|
+        app.filename.include? name
+      end
+    end
+    
+    # Returns the default library (if any) for this system
+    # See Apple's support document (http://support.apple.com/kb/ht1391) as to where libraries can be found
+    def self.default
+      
+      # Use the homedir provided through the environment
+      homedir = ENV['HOME']
+      
+      # List all locations
+      locations = [
+        
+        # Mac OSX and Windows Vista
+        "#{homedir}/Music/iTunes/iTunes Media/Mobile Applications/",
+        
+        # Windows 7
+        "#{homedir}/My Music/iTunes/iTunes Media/Mobile Applications/",
+        
+        # Windows XP
+        "#{homedir}/My Documents/My Music/iTunes/iTunes Media/Mobile Applications/"
+      ]
+      
+      # Raise exception if no default library could be found
+      path = Dir.glob(locations).first
+      unless path
+        raise DoesNotExist, 'Could not locate default iTunes library'
+      end
+      
+      # Return an instance of this default library
+      Library.new path
+    end
+    
+    # Raised when a library does not exist
     class DoesNotExist < StandardError; end
     
   end
