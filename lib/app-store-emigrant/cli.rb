dd8694766013eaf50935b6fb62bd9b2d983cc88b
Tim Kurvers <tim@moonsphere.net>
2012-02-19 22:11:25 +0100

    Added initial implementation of CLI using rainbow-gem

--- lib/app-store-emigrant/cli.rb
+++ lib/app-store-emigrant/cli.rb
@@ -1,14 +1,32 @@
+require 'rainbow'
 
-module AppStore
+module AppStore; end
+
+module AppStore::Emigrant
   
-  module Emigrant
+  # Represents the command line interface
+  class CLI
     
-    class CLI
+    # Initializes CLI instance with given arguments
+    def initialize args
       
-      def initialize args
+      # Extract the path (if any)
+      path, = args
+      
+      # Use the default library on this system when no path is specified
+      library = path ? Library.new(path) : Library.default
+      
+      # Loop through all applications
+      library.apps.each do |app|
         
-        p 'initializing', args
+        # And print their name, version and whether it's outdated
+        print app.name + ' @ ' + "v#{app.version}".foreground(app.outdated? ? :red : :green)
+        if app.outdated?
+          print " (v#{app.cloudversion} available in the cloud)" 
+        end
+        puts
         
+        sleep(1)
       end
       
     end
